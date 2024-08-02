e5a054f3c466e6d257b336687e7a971461d4dc05
K Mahmmad Rafi gari Mustafa <127774530+Mustu122@users.noreply.github.com>
2024-08-02 17:26:41 +0530

    Add files via upload

---
+++ Pri.java
@@ -0,0 +1,16 @@
+/**
+ * Pri
+ */
+public class Pri {
+
+
+    private String name="mustafa";
+    private int age=24;
+    public static void main(String[] args) {
+        Pri obj=new Pri();
+        System.out.println(obj.name);
+        System.out.println(obj.age);
+    }
+
+    
+}
