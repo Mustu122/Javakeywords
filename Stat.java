0f5504c82282becb183ee123394398a7d7e9e923
K Mahmmad Rafi gari Mustafa <127774530+Mustu122@users.noreply.github.com>
2024-08-02 18:18:41 +0530

    Add files via upload

---
+++ Stat.java
@@ -0,0 +1,17 @@
+public class Stat {
+
+    static void mymethod(){
+        System.out.println("hiiii");
+
+    
+    }
+public void methd(){
+
+System.out.println("hey hiiii");
+}
+public static void main(String[] args) {
+    mymethod();
+    Stat obj=new Stat();
+    obj.methd();
+}    
+}
