cb98a176b6054c904013b9a4d5b91be06e90578a
K Mahmmad Rafi gari Mustafa <127774530+Mustu122@users.noreply.github.com>
2024-08-02 18:31:34 +0530

    Add files via upload

---
+++ Clas.java
@@ -0,0 +1,11 @@
+public class Clas {
+
+    int x=12;
+    String y="mustu";
+
+    public static void main(String[] args) {
+        Clas obj=new Clas();
+        System.out.println(obj.x);
+        System.out.println(obj.y);
+    }
+}
