b0803031c03d64e1779607fff97242f85d8aa62b
K Mahmmad Rafi gari Mustafa <127774530+Mustu122@users.noreply.github.com>
2024-08-03 14:35:36 +0530

    Add files via upload

---
+++ iff.java
@@ -0,0 +1,10 @@
+public class iff {
+    public static void main(String[] args) {
+        int a=12;
+        int b=23;
+        if (a<b) {
+  System.out.println("hiiiiii");            
+        }
+    }
+    
+}
