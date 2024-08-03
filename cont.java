fe6505db3c96d3ff6f3439e90a213429f8899494
K Mahmmad Rafi gari Mustafa <127774530+Mustu122@users.noreply.github.com>
2024-08-03 16:34:40 +0530

    Add files via upload

---
+++ cont.java
@@ -0,0 +1,13 @@
+public class cont {
+    public static void main(String[] args) {
+        for(int i=0;i<=20;i++){
+            System.out.println(i);
+            if (i==20) 
+            {
+                continue;
+                
+            }
+        }
+    }
+    
+}
