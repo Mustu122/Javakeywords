0c5d01f25776aee1af16c4c0d7d4917874d2c082
K Mahmmad Rafi gari Mustafa <127774530+Mustu122@users.noreply.github.com>
2024-08-03 14:49:05 +0530

    Add files via upload

---
+++ tr.java
@@ -0,0 +1,15 @@
+public class tr {
+    public static void main(String[] args) {
+        try{
+        int a=1/0;
+        System.out.println(a);
+
+        }
+        catch(Exception e){
+            System.out.println("arthimaticException");
+
+
+        }
+    }
+    
+}
