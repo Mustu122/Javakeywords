9ea1ebbdb2628f4f92c2bc08e061bf5e845fa88b
K Mahmmad Rafi gari Mustafa <127774530+Mustu122@users.noreply.github.com>
2024-08-03 14:40:17 +0530

    Add files via upload

---
+++ Ife.java
@@ -0,0 +1,15 @@
+public class Ife {
+    public static void main(String[] args) {
+        int a=20;
+        int b=30;
+
+        if (a<b) {
+            System.out.println("A IS lessthan THAN B");
+            
+        }
+        else{
+            System.out.println("a is greterthan b");
+        }
+    }
+    
+}
