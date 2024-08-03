e56450f603d7dfdeef9102f0d393dde07d2611b7
K Mahmmad Rafi gari Mustafa <127774530+Mustu122@users.noreply.github.com>
2024-08-03 16:18:05 +0530

    Add files via upload

---
+++ Doowhi.java
@@ -0,0 +1,12 @@
+public class Doowhi {
+    public static void main(String[] args) {
+        int a=1;
+        do{
+            System.out.println(a);
+            a++;
+
+        }
+        while(a<=5);
+    }
+    
+}
